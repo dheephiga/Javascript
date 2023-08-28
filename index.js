2eff33bf22fe287cc58607b424e2817fcc44b937
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-28 19:54:08 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.log("this will print the code in to the console tab of the bropwser");
 
 window.alert("POPUP MESSAGE"); // this will show a alert popup in the browser
+
+let age =21;
+console.log(age); 
+
