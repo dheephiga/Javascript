43b43bfb727f05931e939b6f2bfdd07fa36b92c6
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-31 14:18:12 +0530

    modified:   index.html 	modified:   str.js

--- str.js
+++ str.js
@@ -1,4 +1,12 @@
-let uname = "Joey";
+let uname = "Joeeey";
+let phno = "123-3453-5252-243";
+//uname.length();
+document.getElementById("str").innerHTML = "The length of the string is " +uname.length;
 
-uname.slice
-document.getElementById("str").innerHTML = uname;
+console.log(uname.length);
+console.log(uname.charAt(3));
+console.log(uname.indexOf('o'));
+console.log(uname.lastIndexOf('e'));
+console.log(uname.toUpperCase());
+console.log(uname.substring);
+console.log(phno.replaceAll('-',""));
