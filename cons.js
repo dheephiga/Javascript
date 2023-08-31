38aeb1850c25242b22b3c2c35e0ba3f21844820f
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-31 12:23:30 +0530

    modified:   cons.js

--- cons.js
+++ cons.js
@@ -4,3 +4,10 @@
 // r = window.prompt("Radius: ");
 // r = (Number(r));
 // console.log(circum = r * pi);
+
+let a = 4.64;
+
+//a = Math.floor(a); //lower
+console.log(a);
+a = Math.ceil(a); // higher
+console.log(a);
