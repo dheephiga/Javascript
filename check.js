5d8d4d3f34933dbb3408c94100a914b43f1ecb30
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-31 15:34:55 +0530

    new file:   check.js 	modified:   condn.js 	modified:   index.html

---
+++ check.js
@@ -0,0 +1,8 @@
+document.getElementById("myButton").onclick = function()
+{
+    if(document.getElementById("check ").checked)
+        console.log("Subscribed");
+
+    else
+    console.log("Not subscribed");
+}
--- condn.js
+++ condn.js
@@ -1,10 +1,16 @@
-let age = 43;
+// let age = -7;
 
-if(age >= 18) {
-    console.log("You are an adult");
+// if(age >= 18) {
+//     console.log("You are an adult");
+// }
+// else if(age<0) console.log("you havent born yet");
+// else
+// console.log("No you are not an adult");
+
+// console.log(age >= 18 ? "adult" : "child");
+
+let userOnline = true;
+
+if (userOnline) {
+    console.log("user online");
 }
-
-else 
-console.log("No you are not an adult");
-
-console.log(age >= 18 ? "adult" : "child");
