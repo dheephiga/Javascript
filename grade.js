41c5c65efacc5ca546ebc36cfe7cf3532b95e8e5
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-31 20:23:10 +0530

    new file:   fn.js 	modified:   grade.js 	modified:   index.html

---
+++ grade.js
@@ -0,0 +1,15 @@
+let grade = 'B';
+
+switch (grade) {
+    case "A":
+        console.log("You did great");
+        break;
+    
+    case "B":
+        console.log("You did good");
+        break;
+    
+        
+    default:
+        break;
+}
