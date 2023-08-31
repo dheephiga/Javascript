947dc45de0d80d830cec26636d68c8272237e953
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-31 15:48:36 +0530

    modified:   check.js 	modified:   index.html

--- check.js
+++ check.js
@@ -1,8 +1,26 @@
 document.getElementById("myButton").onclick = function()
 {
-    if(document.getElementById("check ").checked)
+    const visa = document.getElementById("visa");
+const mastercard = document.getElementById("mastercard");
+const paypal = document.getElementById("paypal");
+    if(document.getElementById("check").checked)
         console.log("Subscribed");
 
     else
     console.log("Not subscribed");
+
+    if(visa.checked)
+    console.log("You are paying with visa card");
+
+    else if(mastercard.checked)
+    console.log("You are using mastercard");
+
+    else if(paypal.checked)
+    console.log("You are using paypal");
+
+    else 
+    console.log("use a valid method");
 }
+
+
+
