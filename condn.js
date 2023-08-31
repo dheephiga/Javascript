df5fb788cb48d8e239111bfe1cac27b584a9bb70
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-08-31 14:27:20 +0530

    new file:   condn.js 	modified:   index.html 	modified:   str.js

---
+++ condn.js
@@ -0,0 +1,10 @@
+let age = 43;
+
+if(age >= 18) {
+    console.log("You are an adult");
+}
+
+else 
+console.log("No you are not an adult");
+
+console.log(age >= 18 ? "adult" : "child");
--- str.js
+++ str.js
@@ -1,12 +1,27 @@
-let uname = "Joeeey";
-let phno = "123-3453-5252-243";
-//uname.length();
-document.getElementById("str").innerHTML = "The length of the string is " +uname.length;
+// let uname = "Joeeey";
+// let phno = "123-3453-5252-243";
+// //uname.length();
+// // document.getElementById("str").innerHTML = "The length of the string is " +uname.length;
 
-console.log(uname.length);
-console.log(uname.charAt(3));
-console.log(uname.indexOf('o'));
-console.log(uname.lastIndexOf('e'));
-console.log(uname.toUpperCase());
-console.log(uname.substring);
-console.log(phno.replaceAll('-',""));
+// // console.log(uname.length);
+// // console.log(uname.charAt(3));
+// // console.log(uname.indexOf('o'));
+// // console.log(uname.lastIndexOf('e'));
+// // console.log(uname.toUpperCase());
+// // console.log(uname.substring);
+// // console.log(phno.replaceAll('-',"")); 
+
+// let fullname = "John Doe";
+// let fname = fullname.slice(0,4);
+// let lname = fullname.slice(4);
+// console.log(fname);
+
+
+// lname = fullname.slice(fullname.indexOf(" ")+1);
+// console.log(lname);
+
+let user = "snoop Doggg";
+
+let ltr = user.charAt(0).toUpperCase();
+
+console.log(ltr);
