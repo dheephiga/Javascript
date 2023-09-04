f5d9906d102f319afde74678890a181cf76e24b2
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-09-04 21:37:30 +0530

    modified:   index.html 	modified:   literals.js

---
+++ literals.js
@@ -0,0 +1,7 @@
+let userName = "John Doe";
+let items = 4;
+let total = 400;
+let text = `Hello ${userName} You have ${items} in your cart Your 
+total is $${total}`;
+
+document.getElementById("myLabel").innerHTML = text;
