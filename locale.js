430f81daf37f17effcb43f567543928a324faf7d
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-09-04 22:22:33 +0530

    modified:   locale.js

---
+++ locale.js
@@ -0,0 +1,10 @@
+let myNum = 123456.8789;
+// myNum = myNum.toLocaleString("en-US",{style:"currency",currency:"USD"});
+
+// myNum = myNum.toLocaleString("hi-IN",{style:"currency",currency:"INR"});
+
+// myNum = myNum.toLocaleString("de-DE",{style:"currency",currency:"EUR"});
+
+myNum = myNu
+
+console.log(myNum);
