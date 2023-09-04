1216ed8b8a06b5f93de19847646effaa36e91070
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-09-04 19:50:45 +0530

    modified:   tern.js

--- tern.js
+++ tern.js
@@ -5,4 +5,12 @@ console.log(checkAge(age));
 function checkAge(age)
 {
     //age >= 18 ? console.log("true") : console.log("false");
+    return age >= 18 ?true : false;
+}
+
+let win = false;
+//console.log(checkWin(win));
+checkWin(win);
+function checkWin(win){
+    win ? console.log("You won") : console.log("Better Luck Next time");
 }
