3e3b47dd88e12bc804aa765be15e95a2d657dd9c
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-09-05 23:48:48 +0530

    modified:   index.html 	modified:   temp_convert.js

--- temp_convert.js
+++ temp_convert.js
@@ -1,1 +1,20 @@
-l
+document.getElementById("submitButton").onclick = function()
+{
+    let temp = document.getElementById("temp").value;
+  //  let choice = document.getElementById()
+    if(document.getElementById("cButton").checked)
+    {
+        let temp = document.getElementById("temp").value;
+        document.getElementById("result").innerHTML= (temp-32)*(5/9);
+    }
+
+    else if(document.getElementById("fButton").checked)
+    {
+        let temp = document.getElementById("temp").value;
+        document.getElementById("result").innerHTML= (temp*(9/5))+32;
+    }
+    else
+    {
+        document.getElementById("result").innerHTML= "select a unit";
+    }
+}
