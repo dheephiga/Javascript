6627b1c4b0aeaeda74b8377601ad2eabb0bea568
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-09-05 23:54:36 +0530

    modified:   temp_convert.js

--- temp_convert.js
+++ temp_convert.js
@@ -5,14 +5,14 @@ document.getElementById("submitButton").onclick = function()
     if(document.getElementById("cButton").checked)
     {
         let temp = document.getElementById("temp").value;
-        document.getElementById("result").innerHTML= (temp-32)*(5/9);
+        document.getElementById("result").innerHTML= (temp-32)*(5/9) + "°C";
     }
 
     else if(document.getElementById("fButton").checked)
     {
         let temp = document.getElementById("temp").value;
         // document.getElementById("result").innerHTML= (temp*(9/5))+32;
-        document.getElementById("result").innerHTML = faren(temp);
+        document.getElementById("result").innerHTML = faren(temp) + "°F";
     }
     else
     {
