078abb2ff16f23d487b0bd0781268a4876f6b3e9
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-09-06 12:13:14 +0530

    modified:   array.js

--- array.js
+++ array.js
@@ -2,16 +2,16 @@ let fruits = ["apple","mango","guava","orange"];
 
 fruits[0] = "papaya";
 console.log(fruits);
-fruits.push("lemon"); //adds to the end
+fruits.push("lemon");          //adds to the end
 console.log(fruits[2]);
 console.log(fruits);
-fruits.pop(); //delete last element 
+fruits.pop();                  //delete last element 
 console.log(fruits);
 
 fruits.unshift("strawberry"); //adds to the beginning
 console.log(fruits);
 
-fruits.shift(); //removes beginning
+fruits.shift();              //removes beginning
 console.log(fruits);
 
 console.log(fruits.length);
