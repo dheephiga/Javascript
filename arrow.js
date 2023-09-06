54d92041df407c8342e7015259b9d8ae57e37937
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-09-06 15:50:51 +0530

    new file:   arrow.js 	modified:   index.html

---
+++ arrow.js
@@ -0,0 +1,9 @@
+//arrow functions =>
+
+// let greet = (userName) => console.log(`Hello ${userName}`);
+
+// greet("bro");
+
+const percent = (x,y) => { console.log((x/y) *100) ;}
+
+percent(10,100);
